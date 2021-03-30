cb998a48c3ec1846241a50455d65d6a01fb22618
Franz Kohnle <franz@kohnlehome.de>
2021-03-30 11:54:41 +0200

    Add dropdown

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,27 @@
 <template>
   <h1>Home</h1>
+  
+  <label for="parents">Parent: </label>
+
+  <select v-model="selected">
+  <option v-for="parent in parents" v-bind:value="parent.id" v-bind:key="parent.id" id="parents">
+    {{ parent.name }}
+  </option>
+</select>
+<span>Selected: {{ selected }}</span>
 </template>
+
+<script>
+export default {
+  data(){
+    return {
+      selected: 2,
+      parents: [
+        { id: 1, name: "Parent A"},
+        { id: 2, name: "Parent B"},
+        { id: 3, name: "Parent C"},
+      ]
+    }
+  }
+}
+</script>
