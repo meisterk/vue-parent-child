76dffee53a90c685740b5b97fc69ae3b56d14881
meisterk <franz@kohnlehome.de>
2021-06-09 14:04:06 +0200

    Add prop and event

---
+++ src/components/Size.vue
@@ -0,0 +1,32 @@
+<template>
+  <label for="size">Size</label>
+  <select id="size" v-model="size" class="form-control">
+    <option value="150">150 cm</option>
+    <option value="170">170 cm</option>
+    <option value="185">185 cm</option>
+  </select>  
+</template>
+
+<script>
+  export default {
+    name: 'Size',
+    props: { 
+      sizeInCm: Number
+    },
+    data(){
+      return {
+        size: this.sizeInCm
+      }
+    },
+    watch: {
+      size(newValue){
+          console.log(newValue);
+          this.$emit('sizeChanged', newValue);
+      }
+    }
+  };
+</script>
+
+<style>
+  
+</style>
--- src/views/CreateChild.vue
+++ src/views/CreateChild.vue
@@ -6,6 +6,9 @@
     
     <label for="lastName">Lastname</label>
     <input id="lastName" type="text" v-model="lastName" class="form-control">
+    
+    <Size :sizeInCm="size" @sizeChanged="sizeChanged" />
+
     <div class="pt-2">
       <button @click="buttonCancel" class="btn btn-outline-secondary me-2">Cancel</button>
       <button @click="buttonSave" class="btn btn-success">Save</button>
@@ -14,11 +17,16 @@
 </template>
 
 <script>
+import Size from "../components/Size.vue";
 export default {
+  components:{
+    Size
+  },
   data(){   
     return {
         firstName: "",
-        lastName: ""
+        lastName: "",
+        size: 185
     } 
   },
   methods:{
@@ -30,10 +38,14 @@ export default {
         id: null, // new id is calculated in store
         firstName: this.firstName,
         lastName: this.lastName,
+        size: this.size,
         parent: null        
       };
-      this.$store.dispatch('addChild',newChild);
+      this.$store.dispatch('addChild', newChild);
       this.$router.push('/');
+    },
+    sizeChanged(newValue){
+      this.size = newValue;
     }
   }
 }
