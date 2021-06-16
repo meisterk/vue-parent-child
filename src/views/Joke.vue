bac801f28dc4f7bbadbed0e1b6ceffb198b7c159
meisterk <franz@kohnlehome.de>
2021-06-16 12:21:37 +0200

    Add jokes

--- src/views/Joke.vue
+++ src/views/Joke.vue
@@ -1,12 +1,30 @@
 <template>
   <h2>Joke of the day</h2> 
-    <p>Programmer: A machine that turns coffee into code.</p>
+    <p>{{ jokes[randomIndex] }}</p>
     <router-link to="/">Home</router-link>
 </template>
 
 <script>
 export default {
-  name: "Joke"
+  name: "Joke",
+  data(){
+    return {
+      jokes: [
+        "Programmer: A machine that turns coffee into code.",
+        "Programmer: A person who fixed a problem that you don't know you have, in a way you don't understand.",
+        "Algorithm: Word used by programmers when they do not want to explain what they did."
+      ],
+      randomIndex: 0
+    }
+  },
+  methods: {
+    shuffle(){
+      this.randomIndex = Math.floor(Math.random() * 3);
+    }
+  },
+  mounted(){
+    this.shuffle();    
+  }  
 };
 </script>
 
